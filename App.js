61929fdf580498f24c1c892a655b3cdb11a33ec0
Daniel-Murphy33 <murphydaniel10@gmail.com>
2023-05-04 12:27:03 +0100

    assign all and add members done for teams

--- App.js
+++ App.js
@@ -35,7 +35,6 @@ import AssignedNutritionScreen from './screens/loggedIn/NutritionTab/AssignedNut
 import WeightHistoryScreen from './screens/loggedIn/AnalyticsTab/WeightHistoryScreen';
 import TeamScreen from './screens/loggedIn/ProfileTab/TeamScreen';
 import AssignAllScreen from'./screens/loggedIn/ProfileTab/AssignAllScreen'
-import AddMembersScreen from './screens/loggedIn/ProfileTab/AddMembersScreen'
 
 export default function App() {
 
@@ -140,10 +139,6 @@ export default function App() {
               component={TeamScreen}
               options={{headerShown:false}}
             />
-            <Stack.Screen name="AddMembers"
-              component={AddMembersScreen}
-              options={{headerShown:false}}
-            />
             <Stack.Screen name="AssignAll"
               component={AssignAllScreen}
               options={{headerShown:false}}
@@ -230,10 +225,6 @@ export default function App() {
             />
             <Stack.Screen name="TeamScreen"
               component={TeamScreen}
-              options={{headerShown:false}}
-            />
-            <Stack.Screen name="AddMembers"
-              component={AddMembersScreen}
               options={{headerShown:false}}
             />
             <Stack.Screen name="AssignAll"
--- screens/loggedIn/ProfileTab/TeamScreen.js
+++ screens/loggedIn/ProfileTab/TeamScreen.js
@@ -1,5 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,42 +6,114 @@ import {
   SafeAreaView,
   FlatList,
   TouchableOpacity,
+  Modal,
+  TextInput,
+  Button,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { updateDoc, arrayUnion, doc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { db } from "../../../firebase";
 
 const TeamScreen = ({ route }) => {
   const { id, name, members } = route.params;
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [memberName, setMemberName] = useState("");
+  const [memberEmail, setMemberEmail] = useState("");
+
+  const addMember = async () => {
+    if (memberName.trim() !== "" && memberEmail.trim() !== "") {
+      const newMember = { name: memberName, email: memberEmail };
+  
+      // Get current user
+      const userCred = getAuth().currentUser;
+  
+      // Update the team members in Firestore database
+      const teamRef = doc(db, "teams", userCred.uid, "teams", id);
+  
+      try {
+        await updateDoc(teamRef, {
+          members: arrayUnion(newMember),
+        });
+        setModalVisible(false);
+        setMemberName("");
+        setMemberEmail("");
+        navigation.goBack();
+      } catch (error) {
+        console.error("Error updating team members: ", error);
+      }
+    }
+  };
+  
+  const getMembers = () => {
+    return members;
+  };
+  
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={{ left: 20 }}
-      >
+      <TouchableOpacity onPress={() => navigation.goBack()} style={{ left: 20 }}>
         <Ionicons name="arrow-back" size={30} color="#0792F9" />
       </TouchableOpacity>
       <Text style={styles.title}>{name}</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("AssignAll")}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("AssignAll", {
+        getMembers: getMembers,
+      })}>
         <Text style={styles.buttonText}>Assign All</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("AddMembers")}>
+      <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
         <Text style={styles.buttonText}>Add Members</Text>
       </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Add Member</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setMemberName}
+              value={memberName}
+              placeholder="Name"
+              placeholderTextColor={"grey"}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setMemberEmail}
+              value={memberEmail}
+              placeholder="Email"
+              placeholderTextColor={"grey"}
+              keyboardType="email-address"
+            />
+            <Button title="Add Member" onPress={addMember} />
+            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
       <FlatList
         data={members}
         renderItem={({ item: member }) => (
-        <View style={styles.memberContainer}>
-            <TouchableOpacity style={styles.cardContainer} onPress={() =>
+          <View style={styles.memberContainer}>
+            <TouchableOpacity
+              style={styles.cardContainer}
+              onPress={() =>
                 navigation.navigate("SingleClient", {
                   name: member.name,
                   email: member.email,
                 })
-              }>
+              }
+            >
               <Text style={styles.memberName}>{member.name}</Text>
               <Text style={styles.memberEmail}>{member.email}</Text>
             </TouchableOpacity>
-        </View>
+          </View>
         )}
       />
     </SafeAreaView>
@@ -104,5 +175,42 @@ const styles = StyleSheet.create({
     color: "#FFF",
     fontWeight: "bold",
     textAlign: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 8,
+    paddingRight: 8,
+    width: "100%",
+    marginBottom: 15,
   },
 });
